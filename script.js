2e4cde5984c394c0bf3888aac11d63e837bf0b7f
Helio Souza <heliorr7@gmail.com>
2022-03-31 17:01:09 -0300

    requisito 18

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function activeHeader() {
 }
 
 buttonFormHeader.addEventListener('click', activeHeader);
+
+const checkSubmit = document.getElementById('agreement');
+const buttonSubmit = document.getElementById('submit-btn');
+
+function buttonEnable(event) {
+  // https://www.w3schools.com/jsref/prop_checkbox_checked.asp
+  // https://www.w3schools.com/jsref/prop_pushbutton_disabled.asp
+  console.log(buttonSubmit.disable);
+  if (event.target.checked === true) {
+    buttonSubmit.disabled = false;
+  } else {
+    buttonSubmit.disabled = true;
+  }
+}
+
+checkSubmit.addEventListener('click', buttonEnable);
